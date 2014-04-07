8c0546d8cae4d3462b2a5ff369e5e1f853ffea17
Hugo Duncan <hugo@palletops.com>
2014-04-07 20:28:55 +0000

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.palletops/log-config "0.1.3"
+(defproject com.palletops/log-config "0.1.4-SNAPSHOT"
   :description "Log appenders and middleware for timbre"
   :url "http://github.com/palletops/log-config"
   :license {:name "Eclipse Public License"
