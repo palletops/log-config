c27831f042e1917f6be48979d46369c5236fb71d
Hugo Duncan <hugo@hugoduncan.org>
2014-10-01 18:08:54 -0400

    Remove test alias
    Travis is running lein 2.4.3, which doesn't like the alias.

--- profiles.clj
+++ profiles.clj
@@ -1,10 +1,7 @@
 {:dev {:test-selectors {:default (complement :slf4j)
                         :slf4j :slf4j}
        :plugins [[lein-pallet-release "RELEASE"]]
-       :aliases {"test" ["do"
-                         "test,"
-                         "with-profile" "+logback" "test" ":slf4j"]
-                 "check" ["with-profile" "+logback" "check"]}}
+       :aliases {"check" ["with-profile" "+logback" "check"]}}
  :no-checkouts {:checkout-deps-shares ^{:replace true} []}
  :logback {:dependencies [[org.clojure/tools.logging "0.2.6"]
                           [ch.qos.logback/logback-classic "1.0.9"]]
